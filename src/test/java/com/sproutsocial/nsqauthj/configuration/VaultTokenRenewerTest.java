a2d4756cde826e069b988d875f9123024780bf33
Dave Huber <david.huber@gmail.com>
2022-07-13 08:28:37 -0500

    simplify tests

--- src/test/java/com/sproutsocial/nsqauthj/configuration/VaultTokenRenewerTest.java
+++ src/test/java/com/sproutsocial/nsqauthj/configuration/VaultTokenRenewerTest.java
@@ -5,12 +5,9 @@ import com.bettercloud.vault.VaultException;
 import com.bettercloud.vault.api.Auth;
 import com.bettercloud.vault.response.AuthResponse;
 import com.bettercloud.vault.response.LookupResponse;
-import java.util.concurrent.Delayed;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -27,50 +24,6 @@ import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class VaultTokenRenewerTest {
-
-  private class CompletedScheduledFuture<T> implements ScheduledFuture<T> {
-
-    private final T value;
-
-    public CompletedScheduledFuture(T value) {
-      this.value = value;
-    }
-
-    @Override
-    public long getDelay(TimeUnit unit) {
-      return 0;
-    }
-
-    @Override
-    public int compareTo(Delayed o) {
-      return Long.compare(getDelay(TimeUnit.NANOSECONDS), o.getDelay(TimeUnit.NANOSECONDS));
-    }
-
-    @Override
-    public boolean cancel(boolean mayInterruptIfRunning) {
-      return false;
-    }
-
-    @Override
-    public boolean isCancelled() {
-      return false;
-    }
-
-    @Override
-    public boolean isDone() {
-      return true;
-    }
-
-    @Override
-    public T get() throws InterruptedException, ExecutionException {
-      return value;
-    }
-
-    @Override
-    public T get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
-      return value;
-    }
-  }
 
   Vault vault = mock(Vault.class);
   @Mock Auth auth;
@@ -90,9 +43,10 @@ public class VaultTokenRenewerTest {
   /* does the first scheduled thing immediately on the calling thread, all future scheduled things are dropped */
   private void executeScheduledRunnableImmediatelyOnce() {
     when(scheduledExecutorService.scheduleAtFixedRate(any(Runnable.class), anyLong(), anyLong(), any(TimeUnit.class))).thenAnswer((Answer<ScheduledFuture>) invocationOnMock -> {
+      ScheduledFuture<?> theFuture = mock(ScheduledFuture.class);
       ((Runnable) invocationOnMock.getArgument(0)).run();
-      return new CompletedScheduledFuture<>(null);
-    }).thenReturn(new CompletedScheduledFuture<>(null));
+      return theFuture;
+    }).thenReturn(mock(ScheduledFuture.class));
   }
 
   @Test
