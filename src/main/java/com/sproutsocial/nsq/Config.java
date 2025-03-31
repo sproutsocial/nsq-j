de97a818ee6e83b7339af862cd7ebbd20bcc6b6a
Micah Bennett <micah@sproutsocial.com>
2025-03-31 14:22:36 -0400

    TRG-1790: Release 1.5.0

--- src/main/java/com/sproutsocial/nsq/Config.java
+++ src/main/java/com/sproutsocial/nsq/Config.java
@@ -18,7 +18,7 @@ public class Config {
     private Boolean deflate;
     private Integer deflateLevel;
     private Integer sampleRate;
-    private String userAgent = "nsq-j/1.4.9";
+    private String userAgent = "nsq-j/1.5.0";
     private Integer msgTimeout;
 
     //region accessors
